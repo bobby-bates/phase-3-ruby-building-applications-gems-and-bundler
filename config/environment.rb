464f7e419fb6458178b3c5acf5254d23bbd5181b
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-28 20:40:56 -0400

    complete assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,9 @@
-# Set up Bundler here following the instructions in the README
+# These two lines NEED to be in this order!
+require 'bundler/setup'
+# This code:
+Bundler.require(:default, :development)
+
+# Is equivalent to this code:
+# require 'rspec'
+# require 'rest-client'
+# require 'pry'
